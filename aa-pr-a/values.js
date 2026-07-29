57fd45b3b45ae2850ada935f00e7bc8f3355d4af
akash1810 <akash1810@users.noreply.github.com>
2026-07-29 23:13:16 +0100

    fix: Reset content of gh-pages branch

--- aa-pr-a/values.js
+++
@@ -1,6 +0,0 @@
-  export const GitHubValues = {
-    "githubCommitSha": "17c00541bfa40a5b0eeffd667f0d6664ddf87187",
-    "userCommitSha": "70667dbc1250ca85019397b35af2699b722958fa",
-    "buildNumber": "54",
-    "branchName": "aa-pr-a"
-  };
